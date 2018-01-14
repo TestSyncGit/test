6ce4ef1159a9ab12fb4ad88b60a92183c0865dd6
Alban PRATS <al26130p@gmail.com>
2018-01-14 21:46:39 +0000

    code refactor

--- api/urls.py
+++ api/urls.py
@@ -24,7 +24,6 @@ admin_router.register(r'amountoption', admin.AmountBilletOptionViewSet,'amount-o
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-
     url(r'^authenticate/invitation$', views.InvitationAuthentication.as_view()),
     url(r'^authenticate$', obtain_jwt_token),
     url(r'^authenticate/refresh$', refresh_jwt_token),
